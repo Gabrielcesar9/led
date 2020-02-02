dfb3e75bfbdbf34b4855305d3f0fcc838491d05a
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-02 21:57:51 -0200

    1

--- src/api.js
+++ src/api.js
@@ -2,14 +2,14 @@ const express = require('express');
 const serverless = require('serverless-http')
 const Datastore = require('nedb');
 app.use(express.static('public'));
-const database= newDatastore('database.db');
+const database= new Datastore('database.db');
 database.loadDatabase();
 
 const app = express();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
     res.json({'hello':'hi!'});
 });
 app.use('/.netlify/functions/api', router);
