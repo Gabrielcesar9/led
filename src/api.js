9233960cfc53a074f1c2e879db641a66333d20d5
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-03 01:06:36 -0200

    lambda airtable

--- src/api.js
+++ src/api.js
@@ -5,13 +5,13 @@ const serverless = require('serverless-http')
 //const database= new Datastore('database.db');
 //database.loadDatabase();
 
-/*var Airtable = require('airtable');
+var Airtable = require('airtable');
 var base = new Airtable({apiKey:'keyFqKmXCtU2IkZGw'}).base('app1ANJMB2FcVdb5o');
 
 base('Table 1').find('recRswfevsMQarIVS', function(err, record) {
     if (err) { console.error(err); return; }
     console.log('Retrieved', record.id);
-});*/
+});
 console.log('loglog is working');
 const app = express();
 
