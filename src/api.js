9ac2bf11f89be66940296276e5e8e36742311e8f
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-03 15:13:01 -0200

    airtable

--- src/api.js
+++ src/api.js
@@ -9,15 +9,35 @@ const base = Airtable.base('app1ANJMB2FcVdb5o')
 
 exports.handler = function(event, context, callback){
     const allRecords = []
-    base('Table 1').find('recRswfevsMQarIVS', function(err, record) {
-        allRecords.push(record)
+    base('Table 1')
+    .select({
+      maxRecords: 100,
+      view: 'all'
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          allRecords.push(record)
+        })
+        fetchNextPage()
+      },
+      function done(err) {
+        if (err) {
+          callback(err)
+        } else {
+          const body = JSON.stringify({ records: allRecords })
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              'content-type': 'application/json',
+              'cache-control': 'Cache-Control: max-age=300, public'
+            }
+          }
+          callback(null, response)
         }
+      }
     )
-
-    callback(null, {
-        statusCode:200,
-        body: (JSON.stringify({records:allRecords}))
-    });
 }
 const app = express();
 const router = express.Router();
