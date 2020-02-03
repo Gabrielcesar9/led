264e45cbd5fa33f3527558e15de66dc71b8517be
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-03 14:21:32 -0200

    airtable

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
+const express = require('express');
 exports.handler = function(event, context, callback){
     callbackl(null, {
         statusCode:200,
         body: 'Hello World'
     });
 }
+const app = express();
+const router = express.Router();
+app.use('/.netlify/functions/api', router);
