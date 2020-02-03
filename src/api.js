be35fe734ce527794165b52725e7ea87bf8c4d84
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-03 14:39:11 -0200

    airtable

--- src/api.js
+++ src/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const Airtable = require('airtable')
+Airtable.configure({
+    endpointUrl:'http://api.airtable.com',
+    apiKey: process.env.API_KEY
+})
+const base = AIRtable.base('app1ANJMB2FcVdb5o')
+
 exports.handler = function(event, context, callback){
     callback(null, {
         statusCode:200,
-        body: 'Hello World'
+        body: (console.log('this is pau'))
     });
 }
 const app = express();
