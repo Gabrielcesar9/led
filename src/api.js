25bdfa9e62a050113fe807b76bae69115d6db4a5
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-03 23:38:58 -0200

    commit all

--- src/api.js
+++ src/api.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const Airtable = require('airtable')
-Airtable.configure({
+/*Airtable.configure({
     endpointUrl:'http://api.airtable.com',
-    apiKey: process.env.API_KEY
+    apiKey: 'keyFqKmXCtU2IkZGw'
 })
 const base = Airtable.base('app1ANJMB2FcVdb5o')
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyFqKmXCtU2IkZGw'}).base('app1ANJMB2FcVdb5o');*/
 
-
-exports.handler = function(event, context, callback){
+exports.handler = function(event, context, callback) {
+    const {API_URL, API_CLIENT_ID, API_KEY } = process.env;
+    Airtable.configure({
+    endpointUrl:API_URL,
+    apiKey: API_KEY
+})
+    var base = Airtable.base(API_CLIENT_ID);
     const allRecords = []
     base('Table 1')
     .select({
       maxRecords: 100,
-      view: 'all'
+      view: 'Grid view'
     })
     .eachPage(
       function page(records, fetchNextPage) {
@@ -25,7 +33,7 @@ exports.handler = function(event, context, callback){
         if (err) {
           callback(err)
         } else {
-          const body = JSON.stringify({ records: allRecords })
+          const body = JSON.parse(allRecords)
           const response = {
             statusCode: 200,
             body: body,
@@ -38,7 +46,10 @@ exports.handler = function(event, context, callback){
         }
       }
     )
-}
+      
+  }
 const app = express();
+
 const router = express.Router();
 app.use('/.netlify/functions/api', router);
+
