b93c67ac6736c2511634e3c33d3d6d1854614075
Gabriel Cesar <gabriel.cesar.pereira.9@gmail.com>
2020-02-08 09:29:00 -0200

    f

--- scripts/login.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-    const v = document.getElementById("login").value;
-    const p = document.getElementById("pass").value;
-    console.log(v);
-    console.log(p)
-}
---
+++ src/login.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.all("/", function(req, res, next) {
+  req.header("Origin", "*"); // ideally the '*' will be your hostname
+  return next();
+});
+const serverless = require('serverless-http');
+
+
+exports.handler = function(event, context, callback) {
+  console.log('event.body',event.body);
+  var entries = JSON.parse(event.body)
+  console.log(entries.username)
+    response = {
+            statusCode: 200,
+            body:'bode',
+            headers: {
+              'content-type': 'application/json',
+              'cache-control': 'Cache-Control: max-age=300, public'
+            }};
+            callback(null, response)
+    
+}
